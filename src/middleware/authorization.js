0f7af551487e7eecf6663e794eee8d0c20e9bcc2
Sahil-Chavan <sahiledupor@gmail.com>
2020-04-19 23:34:16 +0530

    Express middleware created

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -3,14 +3,17 @@ const User = require('../models/user')
 
 const authorization = async (req,res,next) =>{
 try {
-      const token = req.header('Authoriz  ation').replace('Bearer ','')
+      const token = req.header('Authorization').replace('Bearer ','')
       const decode = jwt.verify(token,'nodejs')
       const user = await User.findOne({_id:decode._id,'tokens.token':token})
       if(!user){
           throw new Error('Authorization failed')
       }
       req.user = user
+      req.token = token
+      
       next()
+
 } catch (e) {
     res.status(401).send(e.message)
 }
